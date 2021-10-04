88369a03cfd24652583e8d19461ac86676b8878c
bozonhiggsa <ihor.v.savchenko@gmail.com>
2021-10-04 23:43:57 +0300

    Initial commit

---
+++ server/server.py
@@ -0,0 +1,97 @@
+import io
+import os
+import cv2
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+import uvicorn
+import numpy as np
+import nest_asyncio
+from enum import Enum
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import StreamingResponse
+
+# Assign an instance of the FastAPI class to the variable "app".
+# You will interact with your api using this instance.
+app = FastAPI(title='Deploying a ML Model with FastAPI')
+
+
+# List available models using Enum for convenience. This is useful when the options are pre-defined.
+class Model(str, Enum):
+    yolov3tiny = "yolov3-tiny"
+    yolov3 = "yolov3"
+
+
+# By using @app.get("/") you are allowing the GET method to work for the / endpoint.
+@app.get("/")
+def home():
+    return "Congratulations! Your API is working as expected. Now head over to http://localhost:8000/docs."
+
+
+# This endpoint handles all the logic necessary for the object detection to work.
+# It requires the desired model and the image in which to perform object detection.
+@app.post("/predict")
+def prediction(model: Model, file: UploadFile = File(...)):
+    # 1. VALIDATE INPUT FILE
+    filename = file.filename
+    print("point1")
+    fileExtension = filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    if not fileExtension:
+        raise HTTPException(status_code=415, detail="Unsupported file provided.")
+
+    # 2. TRANSFORM RAW IMAGE INTO CV2 image
+
+    # Read image as a stream of bytes
+    image_stream = io.BytesIO(file.file.read())
+    print("point2")
+
+    # Start the stream from the beginning (position zero)
+    image_stream.seek(0)
+    print("point3")
+
+    # Write the stream of bytes into a numpy array
+    file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
+    print("point4")
+
+    # Decode the numpy array as an image
+    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    print("point5")
+
+    # 3. RUN OBJECT DETECTION MODEL
+
+    # Run object detection
+    bbox, label, conf = cv.detect_common_objects(image, model=model)
+    print("point6")
+
+    # Create image that includes bounding boxes and labels
+    output_image = draw_bbox(image, bbox, label, conf)
+    print("point7")
+
+    dir_name = "images_uploaded"
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+    # Save it in a folder within the server
+    cv2.imwrite(f'images_uploaded/{filename}', output_image)
+    print("point8")
+
+    # 4. STREAM THE RESPONSE BACK TO THE CLIENT
+
+    # Open the saved image for reading in binary mode
+    file_image = open(f'images_uploaded/{filename}', mode="rb")
+    #file_image = open('meeting7.jpg', mode="rb")
+    print("point9")
+
+    # Return the image as a stream specifying media type
+    return StreamingResponse(file_image, media_type="image/jpeg")
+
+
+if __name__ == "__main__":
+    # Allows the server to be run in this interactive environment
+    nest_asyncio.apply()
+    print("point0")
+
+    # Spin up the server!
+    uvicorn.run("server:app", host="0.0.0.0", port=8000)
+
+
+
